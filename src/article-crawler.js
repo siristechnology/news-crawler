3eefdec497a602c3df7bd0a9c7b86e97d6ca22e4
Shiva Aryal <shivaaryalj7@gmail.com>
2020-09-23 18:48:40 +0545

    closing browser when crashed

--- src/article-crawler.js
+++ src/article-crawler.js
@@ -6,72 +6,77 @@ const htmlToText = require('html-to-text')
 module.exports = async function (sourceConfigs, { articleUrlLength = 3, headless = true }) {
 	const browser = await getBrowser({ headless })
 	const browserPage = (await browser.pages())[0]
-	await browserPage.setDefaultNavigationTimeout(0)
-	await browserPage.emulate(device)
+	try{
+		await browserPage.setDefaultNavigationTimeout(0)
+		await browserPage.emulate(device)
 
-	let articles = []
+		let articles = []
 
-	for (const source of sourceConfigs) {
-		const articleSelectors = source['article-detail-selectors']
+		for (const source of sourceConfigs) {
+			const articleSelectors = source['article-detail-selectors']
 
-		for (const page of source.pages) {
-			await browserPage.goto(page.url)
+			for (const page of source.pages) {
+				await browserPage.goto(page.url)
 
-			const linkSelector = page['link-selector']
-			const articleUrls = await browserPage.$$eval(linkSelector, (elements) => elements.map((element) => element.href))
-			for (const articleUrl of articleUrls.slice(0, articleUrlLength)) {
-				try {
-					await browserPage.goto(articleUrl, {
-						waitUntil: 'load',
-						// Remove the timeout
-						timeout: 0,
-					})
-
-					const title = await browserPage.$eval(articleSelectors.title, (element) => element.textContent)
-					const excerpt = await browserPage.$eval(articleSelectors.excerpt, (element) => element.textContent)
-					const leadImage = await browserPage.$eval(articleSelectors['lead-image'], (element) => element.src)
-					let content = []
-					for (const contentSelector of articleSelectors.content) {
-						const innerContent = await browserPage.$$eval(contentSelector, (elements) => elements.map((element) => element.innerHTML))
-						content.push(innerContent)
-					}
-
-					content = htmlToText
-						.fromString(content, {
-							wordwrap: false,
-							ignoreImage: true,
-							ignoreHref: true,
-							ignoreImage: true,
-							preserveNewlines: false,
+				const linkSelector = page['link-selector']
+				const articleUrls = await browserPage.$$eval(linkSelector, (elements) => elements.map((element) => element.href))
+				for (const articleUrl of articleUrls.slice(0, articleUrlLength)) {
+					try {
+						await browserPage.goto(articleUrl, {
+							waitUntil: 'load',
+							// Remove the timeout
+							timeout: 0,
 						})
-						.trim()
-						.slice(0, 2000)
 
-					let article = {
-						sourceName: source.sourceName,
-						category: page.category,
-						url: articleUrl,
-						title,
-						shortDescription: excerpt,
-						imageLink: leadImage,
-						isHeadline: true,
-						content: content,
-						createdDate: source.crawlTime,
-						modifiedDate: source.crawlTime,
-						publishedDate: source.crawlTime,
-						link: articleUrl,
-						topic: page.category,
+						const title = await browserPage.$eval(articleSelectors.title, (element) => element.textContent)
+						const excerpt = await browserPage.$eval(articleSelectors.excerpt, (element) => element.textContent)
+						const leadImage = await browserPage.$eval(articleSelectors['lead-image'], (element) => element.src)
+						let content = []
+						for (const contentSelector of articleSelectors.content) {
+							const innerContent = await browserPage.$$eval(contentSelector, (elements) => elements.map((element) => element.innerHTML))
+							content.push(innerContent)
+						}
+
+						content = htmlToText
+							.fromString(content, {
+								wordwrap: false,
+								ignoreImage: true,
+								ignoreHref: true,
+								ignoreImage: true,
+								preserveNewlines: false,
+							})
+							.trim()
+							.slice(0, 2000)
+
+						let article = {
+							sourceName: source.sourceName,
+							category: page.category,
+							url: articleUrl,
+							title,
+							shortDescription: excerpt,
+							imageLink: leadImage,
+							isHeadline: true,
+							content: content,
+							createdDate: source.crawlTime,
+							modifiedDate: source.crawlTime,
+							publishedDate: source.crawlTime,
+							link: articleUrl,
+							topic: page.category,
+						}
+
+						articles.push(article)
+					} catch (error) {
+						console.log('error produced article crawl', error.message, articleUrl)
 					}
-
-					articles.push(article)
-				} catch (error) {
-					console.log('error produced article crawl', error.message, articleUrl)
 				}
 			}
 		}
+
+		await browser.close()
+
+		return articles
+	}catch{
+		await browser.close()
+		return []
 	}
-
-	await browser.close()
-
-	return articles
 }
