6878aea64c4e907d6e6421891c3e96932172b378
Shiva Aryal <lostinhell99@gmail.com>
2020-10-02 23:33:52 +0545

    Image crawl (#6)
    * image only crawling articles added
    
    * image only crawling articles added
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/article-crawler.js
+++ src/article-crawler.js
@@ -21,54 +21,72 @@ module.exports = async function (sourceConfigs, { maxArticlesPerPage, articleUrl
 				await browserPage.goto(page.url)
 
 				const linkSelector = page['link-selector']
-				const articleUrls = await browserPage.$$eval(linkSelector, (elements) => elements.map((element) => element.href))
-				for (const articleUrl of articleUrls.slice(0, maxArticlesPerPage)) {
-					try {
-						await browserPage.goto(articleUrl, {
-							waitUntil: 'load',
-							// Remove the timeout
-							timeout: 0,
-						})
-
-						const title = await browserPage.$eval(articleSelectors.title, (element) => element.textContent)
-						const excerpt = await browserPage.$eval(articleSelectors.excerpt, (element) => element.textContent)
-						const leadImage = await browserPage.$eval(articleSelectors['lead-image'], (element) => element.src)
-						let content = []
-						for (const contentSelector of articleSelectors.content) {
-							const innerContent = await browserPage.$$eval(contentSelector, (elements) => elements.map((element) => element.innerHTML))
-							content.push(innerContent)
-						}
-
-						content = htmlToText
-							.fromString(content, {
-								wordwrap: false,
-								ignoreImage: true,
-								ignoreHref: true,
-								ignoreImage: true,
-								preserveNewlines: false,
+				if(linkSelector){
+					const articleUrls = await browserPage.$$eval(linkSelector, (elements) => elements.map((element) => element.href))
+					for (const articleUrl of articleUrls.slice(0, maxArticlesPerPage)) {
+						try {
+							await browserPage.goto(articleUrl, {
+								waitUntil: 'load',
+								// Remove the timeout
+								timeout: 0,
 							})
-							.trim()
-							.slice(0, 2000)
 
+							const title = await browserPage.$eval(articleSelectors.title, (element) => element.textContent)
+							const excerpt = await browserPage.$eval(articleSelectors.excerpt, (element) => element.textContent)
+							const leadImage = await browserPage.$eval(articleSelectors['lead-image'], (element) => element.src)
+							let content = []
+							for (const contentSelector of articleSelectors.content) {
+								const innerContent = await browserPage.$$eval(contentSelector, (elements) => elements.map((element) => element.innerHTML))
+								content.push(innerContent)
+							}
+
+							content = htmlToText
+								.fromString(content, {
+									wordwrap: false,
+									ignoreImage: true,
+									ignoreHref: true,
+									ignoreImage: true,
+									preserveNewlines: false,
+								})
+								.trim()
+								.slice(0, 2000)
+
+							let article = {
+								sourceName: source.sourceName,
+								category: page.category,
+								url: articleUrl,
+								title,
+								shortDescription: excerpt,
+								imageLink: leadImage,
+								isHeadline: true,
+								content: content,
+								createdDate: source.crawlTime,
+								modifiedDate: source.crawlTime,
+								publishedDate: source.crawlTime,
+								link: articleUrl,
+								topic: page.category,
+							}
+
+							articles.push(article)
+						} catch (error) {
+							console.log('error produced article crawl', error.message, articleUrl)
+						}
+					}
+				}else{
+					const imageSelector = page['image-selector']
+					const imageUrls = await browserPage.$$eval(imageSelector, (elements) => elements.map((element) => element.src))
+					for(const imageUrl of imageUrls.slice(0, maxArticlesPerPage)){
 						let article = {
 							sourceName: source.sourceName,
 							category: page.category,
-							url: articleUrl,
-							title,
-							shortDescription: excerpt,
-							imageLink: leadImage,
+							link: imageUrl,
+							imageLink: imageUrl,
+							title: imageUrl,
+							content: imageUrl,
 							isHeadline: true,
-							content: content,
-							createdDate: source.crawlTime,
-							modifiedDate: source.crawlTime,
-							publishedDate: source.crawlTime,
-							link: articleUrl,
-							topic: page.category,
+							topic: page.category
 						}
-
 						articles.push(article)
-					} catch (error) {
-						console.log('error produced article crawl', error.message, articleUrl)
 					}
 				}
 			}
