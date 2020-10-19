56e02bfaf2d8b05d53b60ee79c4383688c2760ab
Shiva Aryal <lostinhell99@gmail.com>
2020-10-19 18:40:45 +0545

    image test added (#8)
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/article-crawler.js
+++ src/article-crawler.js
@@ -91,7 +91,6 @@ module.exports = async function (sourceConfigs, { maxArticlesPerPage, articleUrl
 						}
 					}
 				} else {
-					console.log('Crawling image..')
 					const imageSelector = page['image-selector']
 					const imageUrls = await browserPage.$$eval(imageSelector, (elements) => elements.map((element) => element.src))
 					for (const imageUrl of imageUrls.slice(0, maxArticlesPerPage)) {
